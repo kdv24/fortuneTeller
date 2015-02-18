35550f996bd4687cb72e7419bfccf2230a5845fc
Kelly de Vries <kdv@me.com>
2015-02-17 17:15:32 -0800

    Add appropriate punctuation and concatenation

--- fortune_teller.php
+++ fortune_teller.php
@@ -4,20 +4,20 @@ $user_favorite_color = $_GET["user_favorite_color"];
 $current_temperature = $_GET["temperature"];
 
 
-$message = tellBirthFortune($user_birthmonth) . " " . tellColorFortune($user_favorite_color) . " " . tellTemperature($current_temperature);
+$message = "I can tell you are both " . tellBirthFortune($user_birthmonth) . " and " . tellColorFortune($user_favorite_color) . ".  I also know " . tellTemperature($current_temperature);
 
 //Need to add CSS code to pretty up HTML doc it returns
 
 function tellBirthFortune($birthday_month)
   {
     if ($birthday_month < 5) {
-      return "You are fabulous!";
+      return "fabulous";
     }
     elseif ($birthday_month > 8) {
-      return "You are super duper!";
+      return "super duper";
     }
     else {
-      return "You are one cool kid!";
+      return "very interesting";
     }
   }
 
@@ -25,19 +25,19 @@ function tellColorFortune($favorite_color)
 //add code to generate background that matches $favorite_color response
   {
     if (($favorite_color == red) || ($favorite_color == blue) || ($favorite_color == yellow)) {
-      return "Your color choice tells me you are a bold decision maker.";
+      return "a bold decision maker";
     }
     elseif (($favorite_color == brown) || ($favorite_color == black)) {
-      return "You are a deep thinker.";
+      return "a deep thinker";
     }
     elseif ($favorite_color == white) {
-      return "The details matter to you.";
+      return "a person who cares about the details";
     }
     elseif (($favorite_color == orange) || ($favorite_color == purple)) {
-      return "You like to be unique in a crowd.";
+      return "you like to be unique in a crowd";
     }
     else {
-      return "You are interesting.";
+      return "you are a free thinker";
     }
   }
 
@@ -45,13 +45,13 @@ function tellTemperature($temperature)
 //add in get date function to check against $temperature
   {
     if ($temperature < 60) {
-      return "It's cold out!";
+      return "it's cold out!";
     }
     elseif ($temperature > 80) {
-      return "It's too hot!";
+      return "it's too hot!";
     }
     else {
-      return "It's a lovely day. Get off your computer and go outside!";
+      return "it's a lovely day, so get off your computer and go outside!";
     }
   }
 ?>
